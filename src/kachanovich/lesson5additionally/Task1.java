b2202c3ed1ff2f08cc55b6d267406548b66635f6
MikKach <89706354+MikKach@users.noreply.github.com>
2022-03-18 15:54:01 +0300

    add lesson5additionally Task1

---
+++ src/kachanovich/lesson5additionally/Task1.java
@@ -0,0 +1,10 @@
+package kachanovich.lesson5additionally;
+
+public class Task1 {
+    /*
+    1) Найти произведение элементов, кратных 3.
+     */
+    public static void main(String[] args) {
+
+    }
+}
