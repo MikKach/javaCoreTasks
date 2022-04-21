bcad554a2744011ca101df85ac0aed299972c054
MikKach <89706354+MikKach@users.noreply.github.com>
2022-04-21 15:49:17 +0300

    add lesson10 task1

---
+++ src/kachanovich/lesson10/examcloudsException/task1/Task1.java
@@ -0,0 +1,18 @@
+package kachanovich.lesson10.examcloudsException.task1;
+
+public class Task1 { //TODO logic
+    private String login;
+    private String password;
+    private String confirmPassword;
+
+    public Task1() {
+    }
+
+    public Task1(String login, String password, String confirmPassword) {
+        this.login = login;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+
+}
--- src/kachanovich/lesson9/testLesson/Test.java
+++
@@ -1,9 +0,0 @@
-package kachanovich.lesson9.testLesson;
-
-public class Test {
-    public static void main(String[] args) {
-        String str = "hello";
-        StringBuilder sb1 = new StringBuilder(str);
-        System.out.println(sb1.append(" world!"));
-    }
-}
