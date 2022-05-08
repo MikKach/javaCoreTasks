a41feb1891b546d80e86fe1e6214287b62e2fb93
MikKach <89706354+MikKach@users.noreply.github.com>
2022-05-08 15:32:56 +0300

    add lesson14 start

---
+++ src/kachanovich/lesson14/examclouds/date/task1/Task1.java
@@ -0,0 +1,7 @@
+package kachanovich.lesson14.examclouds.date.task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/kachanovich/lesson14/examclouds/lambda/task1/Task1.java
@@ -0,0 +1,7 @@
+package kachanovich.lesson14.examclouds.lambda.task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
